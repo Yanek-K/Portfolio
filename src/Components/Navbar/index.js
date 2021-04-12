13b81c13a117bb6dad8e227b11737230e56cda9b
Yanek K <yanek.dev@gmail.com>
2021-04-12 02:53:27 -0400

    Initial Setup

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <p>HOME</p>
+    </div>
+  );
+};
+
+export default Homepage;

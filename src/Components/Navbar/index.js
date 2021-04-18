c0779341e58c70a0bd775f1701325e83807a87c2
Yanek K <yanek.dev@gmail.com>
2021-04-18 07:06:31 -0400

    Navbar and Homepage

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,13 +1,52 @@
 import React from "react";
+import "./index.css";
+import { NavLink } from "react-router-dom";
+
+import logo from "./../../Assets/logo.png";
+import github from "./../../Assets/github.png";
+import linkedin from "./../../Assets/linkedin.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>Home</li>
-        </ul>
+    <div className="wrap">
+      <img src={logo} alt="logo" className="logo" />
+      <nav className="nav">
+        <NavLink
+          to="/"
+          exact
+          className="btn"
+          activeStyle={{ color: "#e55d6f" }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className="btn"
+          activeStyle={{ color: "#e55d6f" }}
+        >
+          Projects
+        </NavLink>
+        <NavLink to="/about" className="btn" activeStyle={{ color: "#e55d6f" }}>
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className="btn"
+          activeStyle={{ color: "#e55d6f" }}
+        >
+          Contact
+        </NavLink>
+        {/* <ul>
+          <li className="btn">Home</li>
+          <li className="btn">Projects</li>
+          <li className="btn">About</li>
+          <li className="btn">Contact</li>
+        </ul> */}
       </nav>
+      <div className="links">
+        <img src={github} alt="github" />
+        <img src={linkedin} alt="linkedin" />
+      </div>
     </div>
   );
 };
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import logo from "./../../Assets/logo.png";
+import "./index.css";
 
 const Homepage = () => {
   return (
-    <div>
-      <p>HOME</p>
+    <div className="home__wrap">
+      <div className="inner-wrap">
+        <img src={logo} alt="logo" className="home__logo" />
+        <div className="home__main">
+          <h2>Yanek K.</h2>
+          <p>Front End Web Developer and Designer</p>
+          <div className="buttons">
+            <div className="projects">My Projects</div>
+            <div className="resume">Resume</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
